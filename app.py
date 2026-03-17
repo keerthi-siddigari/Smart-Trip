911dd82fa8bf0f9aa679cab77911b58747c8377d
keerthi-siddigari <keerthi2775@gmail.com>
2026-03-17 23:01:32 +0530

    Turn off debug mode for production

--- app.py
+++ app.py
@@ -315,4 +315,4 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_USE_SIGNER"] = True    # signs session cookies
 Session(app)
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
