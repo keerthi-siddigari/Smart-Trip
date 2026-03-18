f4f45ca994b674c85ba530be1cd36e66a5801c08
keerthi-siddigari <keerthi2775@gmail.com>
2026-03-18 12:35:05 +0530

    fix: add SSL for Aiven

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ app.config['MYSQL_HOST'] = os.getenv("MYSQL_HOST")
 app.config['MYSQL_USER'] = os.getenv("MYSQL_USER")
 app.config['MYSQL_PASSWORD'] = os.getenv("MYSQL_PASSWORD")
 app.config['MYSQL_DB'] = os.getenv("MYSQL_DB")
-
+app.config['MYSQL_SSL_CA'] = '/etc/ssl/certs/ca-certificates.crt'
 # ----------------- MySQL INSTANCE -----------------
 mysql = MySQL(app)
 app.config["MYSQL_CONNECTION"] = mysql
